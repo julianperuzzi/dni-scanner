08579fd8dface1c88eb846d750ac3aff5dd526e5
julianperuzzi <peru964@gmail.com>
2024-11-26 03:59:35 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,22 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { UserProvider } from './context/UserContext';
-import Login from './components/Login';
-import ScanDni from './components/ScanDni';
-import UserDniData from './components/UserDniData';
-import ProtectedRoute from './components/ProtectedRoute';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import React from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
-const App = () => (
-  <UserProvider>
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/scan"
-          element={
-            <ProtectedRoute>
-              <ScanDni />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/my-data"
-          element={
-            <ProtectedRoute>
-              <UserDniData />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-      <Footer />
-    </Router>
-  </UserProvider>
-);
+function App() {
+  const [data, setData] = React.useState("Not Found");
+
+  return (
+    <>
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text);
+          else setData("Not Found");
+        }}
+      />
+      <p>{data}</p>
+    </>
+  );
+}
 
 export default App;
