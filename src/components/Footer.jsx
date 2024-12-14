5072df6d08c82a52504f6521cf52008c52e65fdb
julianperuzzi <126997697+julianperuzzi@users.noreply.github.com>
2024-12-14 10:44:13 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto text-center">
         <p>&copy; 2024 DNI Scanner. Desarrollo Julian Peruzzi.</p>
         <div className="mt-4">
-          <a href="/terms" className="text-gray-400 hover:text-gray-300 mr-4">Términos y condiciones</a>
-          <a href="/privacy" className="text-gray-400 hover:text-gray-300">Política de privacidad</a>
+          <a href="/" className="text-gray-400 hover:text-gray-300 mr-4">Uso Administrado JulianPeruzzi.dev</a>
+          <a href="www.linkedin.com/in/julianperuzzi" className="text-gray-400 hover:text-gray-300">Linkedin</a>
         </div>
       </div>
     </footer>
