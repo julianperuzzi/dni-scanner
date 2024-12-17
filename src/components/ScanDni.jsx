5859fd8da69fe1b335f2fb30a0f98207c9d0d16a
julianperuzzi <peru964@gmail.com>
2024-12-17 10:44:40 -0300

    Update ScanDni.jsx

--- src/components/ScanDni.jsx
+++ src/components/ScanDni.jsx
@@ -5,9 +5,9 @@ import CameraSelect from "./CameraSelect";
 import BarcodeScanner from "./BarcodeScannerComponent";
 
 function ScanDni() {
-  const [selectedDeviceId, setSelectedDeviceId] = useState(null);
-  const [cameras, setCameras] = useState([]);
-  const [scannedData, setScannedData] = useState("");
+  const [selectedDeviceId, setSelectedDeviceId] = useState(null); // Inicialmente nulo
+  const [cameras, setCameras] = useState([]); // Lista de cámaras
+  const [loadingCameras, setLoadingCameras] = useState(true); // Indicador de carga
   const [notification, setNotification] = useState({ message: "", type: "" });
 
   const { user } = useUser();
@@ -26,24 +26,26 @@ function ScanDni() {
       try {
         const devices = await navigator.mediaDevices.enumerateDevices();
         const videoInputs = devices.filter((device) => device.kind === "videoinput");
-        setCameras(videoInputs);
 
-        // Selecciona la cámara predeterminada si no hay una seleccionada
+        setCameras(videoInputs); // Establece la lista de cámaras
+
         if (videoInputs.length > 0) {
           const savedCamera = localStorage.getItem("selectedCamera");
           const defaultCamera = savedCamera || videoInputs[0].deviceId;
-          setSelectedDeviceId(defaultCamera);
+          setSelectedDeviceId(defaultCamera); // Selecciona cámara por defecto
           localStorage.setItem("selectedCamera", defaultCamera);
         }
       } catch (error) {
         console.error("Error al enumerar dispositivos:", error);
+      } finally {
+        setLoadingCameras(false); // Finaliza el proceso de carga
       }
     };
 
     fetchCameras();
-  }, []); // Solo se ejecuta una vez al montar el componente
+  }, []);
 
-  // Actualiza la cámara seleccionada
+  // Maneja la selección de cámaras
   const handleCameraSelect = (event) => {
     const deviceId = event.target.value;
     setSelectedDeviceId(deviceId);
@@ -53,12 +55,19 @@ function ScanDni() {
   // Maneja los resultados del escáner
   const handleScan = (err, result) => {
     if (result) {
-      setScannedData(result.text);
       navigate("/data", { state: { scannedData: result.text } }); // Redirige a /data con los datos escaneados
     } else if (err) {
       console.error("Error al escanear:", err);
     }
   };
+
+  if (loadingCameras) {
+    return (
+      <div className="flex justify-center items-center h-screen bg-gray-900 text-white">
+        <p>Cargando cámaras...</p>
+      </div>
+    );
+  }
 
   return (
     <div className="bg-gray-900">
@@ -66,7 +75,9 @@ function ScanDni() {
         <h3 className="text-xl font-bold p-2 mt-4 text-gray-300">USER: {user?.username}</h3>
         <CameraSelect cameras={cameras} selectedDeviceId={selectedDeviceId} handleCameraSelect={handleCameraSelect} />
       </div>
-      <BarcodeScanner selectedDeviceId={selectedDeviceId} handleScan={handleScan} />
+      {selectedDeviceId && (
+        <BarcodeScanner selectedDeviceId={selectedDeviceId} handleScan={handleScan} />
+      )}
       {notification.message && (
         <div className={`fixed top-60 left-1/2 transform text-2xl -translate-x-1/2 bg-indigo-900/70 text-white p-6 rounded-md text-center z-70 font-semibold uppercase`}>
           {notification.message}
